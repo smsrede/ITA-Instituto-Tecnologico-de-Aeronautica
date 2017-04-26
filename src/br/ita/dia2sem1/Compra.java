3d54e51b87b9616ebb1ec7ebbd784ba8c9090460
Samuel Menezes de Souza <smsrede@gmail.com>
2017-04-26 16:06:21 -0300

    TesteREbase

--- src/br/ita/dia2sem1/Compra.java
+++ src/br/ita/dia2sem1/Compra.java
@@ -7,27 +7,29 @@ teste de unidades
 public class Compra {
 	int valorTotal;
 	int numeroParcelas;
-	
-	//construtor 1 pg a vista 
-	public Compra(int valor){
+
+	// construtor 1 pg a vista
+	public Compra(int valor) {
 		this.valorTotal = valor;
 		this.numeroParcelas = 1;
 	}
-	//construtor 2 pg a parcelado
-	public Compra(int qtdParcelas, int valorParcela){
+
+	// construtor 2 pg a parcelado
+	public Compra(int qtdParcelas, int valorParcela) {
 		this.numeroParcelas = qtdParcelas;
 		valorTotal = valorParcela * qtdParcelas;
 	}
+
 	public int getValorTotal() {
 		return valorTotal;
 	}
+
 	public int getNumeroParcelas() {
 		return numeroParcelas;
 	}
+
 	public int getValorParcela() {
-		return valorTotal/numeroParcelas;
+		return valorTotal / numeroParcelas;
 	}
-	
-	
+
 }
-	
